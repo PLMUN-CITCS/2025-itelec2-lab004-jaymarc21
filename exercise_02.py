4d3f7c8124840815aff3abf9436e0612e46cabe4
jaymarc21 <custodiojaymarc@_bsitplmun.edu.ph>
2025-02-17 23:43:05 +0800

    Submitting Python Week 02 - Session 01 - Exercise 02

---
+++ exercise_02.py
@@ -0,0 +1,11 @@
+user_integer = int(input("Enter an integer: "))
+user_decimal = float(input("Enter a decimal number: "))
+user_decimal = float(input("Enter a decimal number: "))
+print("Formatted Output using old-style formatting:")
+print("Integer: %d" % user_integer)
+print("Decimal: %.2f" % user_decimal)
+print("String: %s" % user_text)
+print("Formatted Output using f-strings:")
+print(f"Integer: {user_integer}")
+print(f"Decimal: {user_decimal:.2f}")  # Format decimal to two places
+print(f"String: {user_text}")
